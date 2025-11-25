84c5183af30ee4f055e1c5048f64a96ad53868cc
AlbanChatelet <chateletalban20@gmail.com>
2025-11-25 09:45:59 +0100

    feat(header): create header component + html base

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <!-- Le logo sera une image plus tard -->
+        <img src="/logo.png" alt="Cooking Delight Logo" />
+      </div>
+
+      <nav class="main-nav">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Recipes</a></li>
+          <li><a href="#">Cooking Tips</a></li>
+          <li><a href="#">About Us</a></li>
+        </ul>
+      </nav>
+
+      <button class="subscribe-btn">Subscribe</button>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+/* styling soon */
+</style>
