4e1114529530f48be2d214c73f94efda36f3b8e6
AlbanChatelet <chateletalban20@gmail.com>
2025-11-25 11:21:44 +0100

    feat(header) : ajout de site-header et de header-wrapper
    site-header est une classe qui sert à donner un background beige au header, on l'applique sur la balise header
    header-wrapper est un conteneur qui contient tous les éléments du header, avec une largeur définie et un display flex pour que les éléments soient sur la même ligne

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="site-header">
     <div class="header-wrapper">
       <!-- Logo -->
       <div class="logo">
@@ -29,5 +29,22 @@
 <style scoped>
 .logo img {
   height: 50px;
+}
+
+.site-header {
+  background-color: #fff7ec; 
+  border-bottom: 1px solid rgba(0, 0, 0, 0.06);
+}
+
+/* Conteneur principal du header */
+.header-wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 12px 24px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 32px;
 }
 </style>
