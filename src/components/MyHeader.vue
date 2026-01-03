2fec1080294faab2c93e1aec9ce8798b16878a0b
AlbanChatelet <chateletalban20@gmail.com>
2026-01-03 19:42:02 +0100

    feat(header): add mobile burger menu

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -5,20 +5,64 @@
         <img class="c-header__logo" src="../assets/logo.png" alt="Cooking MMI Logo" />
       </RouterLink>
 
-      <nav class="c-header__nav">
+      <!-- Burger (mobile) -->
+      <button
+        class="c-burger"
+        type="button"
+        :aria-expanded="isMenuOpen"
+        aria-label="Open menu"
+        @click="isMenuOpen = !isMenuOpen"
+      >
+        <span class="c-burger__line"></span>
+        <span class="c-burger__line"></span>
+        <span class="c-burger__line"></span>
+      </button>
+
+      <!-- Nav (desktop + mobile panel) -->
+      <nav class="c-header__nav" :class="{ 'is-open': isMenuOpen }">
         <ul class="c-header__list">
-          <li><RouterLink class="c-header__link" to="/">HOME</RouterLink></li>
-          <li><RouterLink class="c-header__link" to="/recipes">RECIPES</RouterLink></li>
-          <li><a class="c-header__link" href="#">COOKING TIPS</a></li>
-          <li><RouterLink class="c-header__link" to="/about">ABOUT US</RouterLink></li>
+          <li><RouterLink class="c-header__link" to="/" @click="closeMenu">HOME</RouterLink></li>
+          <li>
+            <RouterLink class="c-header__link" to="/recipes" @click="closeMenu"
+              >RECIPES</RouterLink
+            >
+          </li>
+          <li><a class="c-header__link" href="#" @click="closeMenu">COOKING TIPS</a></li>
+          <li>
+            <RouterLink class="c-header__link" to="/about" @click="closeMenu"
+              >ABOUT US</RouterLink
+            >
+          </li>
         </ul>
+
+        <!-- CTA dans le panel mobile -->
+        <div class="c-header__cta c-header__cta--mobile">
+          <button class="c-btn" type="button">SUBSCRIBE</button>
+        </div>
       </nav>
 
-      <div class="c-header__cta">
-        <button class="c-btn">SUBSCRIBE</button>
+      <!-- CTA desktop -->
+      <div class="c-header__cta c-header__cta--desktop">
+        <button class="c-btn" type="button">SUBSCRIBE</button>
       </div>
     </div>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+const isMenuOpen = ref(false)
+
+function closeMenu() {
+  isMenuOpen.value = false
+}
+
+// Ferme le menu quand on change de page
+const route = useRoute()
+watch(
+  () => route.fullPath,
+  () => closeMenu(),
+)
+</script>
